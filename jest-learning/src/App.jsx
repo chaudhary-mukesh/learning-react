db847869a2791e5eac34112871d2be923ade2d90
chaudhary-mukesh <chaudharymukesh9987@gmail.com>
2025-07-07 14:11:35 +0530

    Initialize jest-learning project with React and Vite setup
    - Created App.css for styling the application.
    - Implemented App component in App.jsx with a simple "Hello World" message.
    - Added sum function for testing purposes.
    - Created unit tests for sum function in App112.test.js and app2.test.js.
    - Added React logo SVG to assets directory.
    - Defined global styles in index.css.
    - Set up main entry point in main.jsx to render the App component.
    - Configured Vite in vite.config.js for React support.

---
+++ jest-learning/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+// Add this function
+export function sum(a, b) {
+  return a + b;
+}
+
+export default App;
---
+++ jest-learning/src/__test__/app2.test.js
@@ -0,0 +1,5 @@
+import { sum } from '../App';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(4, 4)).toBe(8);
+});
